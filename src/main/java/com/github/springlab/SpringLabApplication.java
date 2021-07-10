9759830f2dc0f74d9dd01552b6b7fae56a4651b6
hglee93 <jeuslhg@gmail.com>
2021-07-10 17:36:42 +0900

    init project

---
+++ src/main/java/com/github/springlab/SpringLabApplication.java
@@ -0,0 +1,13 @@
+package com.github.springlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLabApplication.class, args);
+    }
+
+}
