7d8d64bc525ef14ad2336707cf5fdafa5a7bb056
hglee93 <jeuslhg@gmail.com>
2021-07-24 23:31:59 +0900

    CompletableFuture 테스트
    - runAsync로 리턴값이 필요 없는 비동기함수 실행

--- src/test/java/com/github/springlab/completablefuture/AsyncTestTest.java
+++ src/test/java/com/github/springlab/completablefuture/AsyncTestTest.java
@@ -1,14 +1,16 @@
 package com.github.springlab.completablefuture;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CompletableFuture;
-
-import static com.github.springlab.completablefuture.AsyncTest.log;
-import static com.github.springlab.completablefuture.AsyncTest.sleep;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.ExecutionException;
 
 class AsyncTestTest {
+
+    static final Logger logger = LoggerFactory.getLogger(AsyncTestTest.class);
 
     @Test
     void thenApply_테스트() {
@@ -22,5 +24,25 @@ class AsyncTestTest {
         AsyncTest asyncTest = new AsyncTest();
         CompletableFuture acceptTask = asyncTest.asyncThenAcceptTest();
         acceptTask.join();
+    }
+
+    @Test
+    void completableExam01() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+           return "test01";
+        }).thenApply(result -> {
+            String ret = result + " received";
+            System.out.println(ret);
+            return ret;
+        });
+
+        Assertions.assertEquals("test01 received", future.get());
+    }
+
+    @Test
+    void completableExam02() {
+        CompletableFuture.runAsync(() -> {
+            logger.info("call runAsync Comsumer function");
+        });
     }
 }
